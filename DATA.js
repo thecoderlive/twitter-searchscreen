c9b8a6821192473975065ffe0af12a325677e502
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-30 12:58:06 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,62 @@
+export const item = {
+    "id": "694bbf1f-b2d2-4c33-93e6-4c8415c29c00",
+    "trending_news": [
+        {
+            "id": "1c228b1c-a724-4e94-af67-bf687133860b",
+            "trending_news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+        },
+        {
+            "id": "51be8bcf-b574-4a28-a14f-c608ee1484c9",
+            "trending_news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+        },
+        {
+            "id": "7bcb8370-ac61-40ac-b338-10cf0ac6a0a8",
+            "trending_news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+        },
+        {
+            "id": "76de98fa-1be0-4ab0-b568-8514e8c59aeb",
+            "trending_news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+        },
+        {
+            "id": "07250801-c004-48a6-a5bc-756c7011ae07",
+            "trending_news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+        }
+    ],
+    "trending_collection": [
+        {
+            "id": "e6daac88-cee2-4f32-9618-d2b4ada1fb1e",
+            "origin": "phone",
+            "about": "far",
+            "tweet_counts": "account",
+            "news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/248.png"
+        },
+        {
+            "id": "5c87b4ae-84ba-4cc9-a70c-e8a5cf248402",
+            "origin": "pretty",
+            "about": "road",
+            "tweet_counts": "attorney",
+            "news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png"
+        },
+        {
+            "id": "83e770ce-42c9-4077-b751-19a125445484",
+            "origin": "piece",
+            "about": "order",
+            "tweet_counts": "light",
+            "news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+        },
+        {
+            "id": "4ab20c8f-06be-4420-aa5d-ab3e5d64c052",
+            "origin": "perhaps",
+            "about": "recently",
+            "tweet_counts": "laugh",
+            "news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png"
+        },
+        {
+            "id": "ffbfd75c-44d0-46b6-9ff6-ec6359629014",
+            "origin": "develop",
+            "about": "memory",
+            "tweet_counts": "degree",
+            "news_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/103.png"
+        }
+    ]
+}
